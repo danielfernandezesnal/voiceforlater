a22d4ac2da6c566b79a4ae31ec604eec16434d65
Daniel Fernandez Esnal <danielfernandezesnal@gmail.com>
2026-04-03 16:53:26 -0300

    fix: use 'video' field name for video message uploads

--- components/wizard/wizard-client.tsx
+++ components/wizard/wizard-client.tsx
@@ -113,9 +113,13 @@ function WizardContent({ locale, dictionary, userPlan, initialData, messageId, u
             if (data.messageType === 'text') {
                 formData.append('textContent', data.textContent)
             } else if (data.audioBlob) {
-                const filename = 'recording.webm'
-                const fileKey = data.messageType === 'video' ? 'video' : 'audio'
-                formData.append(fileKey, data.audioBlob, filename)
+                if (data.messageType === 'video') {
+                    const filename = data.audioBlob instanceof File ? data.audioBlob.name : 'recording.webm'
+                    formData.append('video', data.audioBlob, filename)
+                } else {
+                    const filename = data.audioBlob instanceof File ? data.audioBlob.name : 'recording.webm'
+                    formData.append('audio', data.audioBlob, filename)
+                }
             }
 
             // Upload photos
